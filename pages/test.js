2f540620b40d474020964926895d4d29745afa1e
sachin_sde <ssachin.s@myclnq.co.in>
2025-06-29 14:37:40 +0530

    routing

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      //http://localhost:3000/test file based routing 
+      <h1>Test page</h1>
+    </div>
+  );
+}
+
+export default Test
