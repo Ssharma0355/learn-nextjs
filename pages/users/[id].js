168bde30a33cc464efadc30d3fc51bcb866aaabb
sachin_sde <ssachin.s@myclnq.co.in>
2025-06-29 15:01:38 +0530

    nestes-routes

---
+++ pages/users/[id].js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SingleUser = () => {
+  return (
+    <div>
+        {/* http://localhost:3000/users/1 file based routing  */}
+        <h1>Single User Page</h1>
+        <p>This page displays the details of a single user.</p>
+        {/* You can add more user-specific information here */}
+      
+    </div>
+  )
+}
+
+export default SingleUser
