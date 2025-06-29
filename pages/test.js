5973b5b5eb6861959b9ba9f616a5dd5eef1da12c
sachin_sde <ssachin.s@myclnq.co.in>
2025-06-29 14:38:07 +0530

    d

--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Test = () => {
   return (
     <div>
-      //http://localhost:3000/test file based routing 
+      {/* http://localhost:3000/test file based routing  */}
       <h1>Test page</h1>
     </div>
   );
