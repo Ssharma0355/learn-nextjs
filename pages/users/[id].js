8dba8183c9078d18397b95e94fe69d282ba45fa9
sachin_sde <ssachin.s@myclnq.co.in>
2025-06-29 15:04:36 +0530

    useRouter

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const SingleUser = () => {
+    const router = useRouter();
   return (
     <div>
         {/* http://localhost:3000/users/1 file based routing  */}
-        <h1>Single User Page</h1>
+        <h1>Single User Page of ID : {router.query.id} </h1>
         <p>This page displays the details of a single user.</p>
         {/* You can add more user-specific information here */}
       
