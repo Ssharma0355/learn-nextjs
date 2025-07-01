c408930e62d2db287f96151a3cc008d6e0a8d2c9
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-01 14:06:01 +0530

    givingtitle

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Footer from "@/components/footer";
 import Layout from "@/components/layout";
 
 export default function Home() {
-  return <Layout>
+  return <Layout title={"Home page"}>
     <h1>Index file</h1>
 
   </Layout>;
